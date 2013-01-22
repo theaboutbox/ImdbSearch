ccdab5ce189656a7cd57f6def0115cb8f507e4f2
Cameron Pope <cameron@theaboutbox.com>
2013-01-22 16:16:31 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IMDBSearch::Application.config.secret_token = '005601faa867964d55d67fe38197d8beb85e0b50a1a69c60e444c577351bbc02769a08dbb6d54ff6b8afaddd8f0956d31156fa2acc305bb6bb7996564ee8f13b'
